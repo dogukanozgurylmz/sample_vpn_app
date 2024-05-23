21df8ae33a4058130682e700a73d44b705ea90a1
Doğukan Özgür Yılmaz <dogukanozgurylmz@gmail.com>
2024-05-23 17:12:03 +0300

    Revert "finish"
    This reverts commit 6ed534087f004cd557b9c699e7e231ea6cf573b0.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:sample_vpn_app/app/cubit/home_cubit.dart';
 import 'package:sample_vpn_app/app/home_view.dart';
 
 void main() {
@@ -14,18 +12,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => HomeCubit()),
-      ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const HomeView(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const HomeView(),
     );
   }
 }
